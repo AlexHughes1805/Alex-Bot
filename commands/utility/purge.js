dc8a98f1a7a8c5c3d3702fd5fef96f799beeea56
Alex Hughes <126358475+AlexHughes1805@users.noreply.github.com>
2024-03-19 15:56:54 +0000

    Update purge.js

--- commands/utility/purge.js
+++ commands/utility/purge.js
@@ -2,19 +2,20 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('purge')
-		.setDescription('deletes specified amount of messages.')
-		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to purge'))
-		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+		.setName('purge') // command name
+		.setDescription('deletes specified amount of messages.') // command description
+		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to purge')) // integer option, the amount of messages memeber wants to delete
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages), // only show command if user has 'manage messages' permission
 		
 	async execute(interaction) {
 		const amount = interaction.options.getInteger('amount');
 
+		//delete amount of messages specified or error message
 		await interaction.channel.bulkDelete(amount, true).catch(error => {
 			console.error(error);
 			interaction.reply({ content: 'There was an error trying to purge messages in this channel!', ephemeral: true });
 		});
 
-		return interaction.reply({ content: `Successfully purged \`${amount}\` messages.`, ephemeral: true });
+		return interaction.reply({ content: `Successfully purged \`${amount}\` messages.`, ephemeral: true }); //confirmation message, only viewable to member who called the command
 	},
 };
